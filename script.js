204b968987d01e526cc76e8f9b824e8e50c70bd7
Victor Santin <157748636+vsanlic@users.noreply.github.com>
2024-06-13 21:06:42 +0200

    Modify script.js to add random color features and clean up code

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const body = document.querySelector("body");
 const container = document.querySelector(".container");
 const input = document.querySelector("input");
 const modify = document.querySelector(".modify-number-squares");
+const randomSquares = document.querySelector(".random-squares");
+const blackSquares = document.querySelector(".black-squares");
 
 function generateDefaultGrid() {
     for (let i = 0; i < 16; i++) {
@@ -16,11 +18,11 @@ function generateDefaultGrid() {
     }
 }
 
-function addMouseEvent() {
+function addDefaultMouseEvent() {
   const allSquares = document.querySelectorAll(".square");
   for (let i = 0; i < allSquares.length; i++) {
     allSquares[i].addEventListener("mouseover", () => {
-      allSquares[i].className = "blackSquare";
+      allSquares[i].style.backgroundColor = "black";
     });
   }
 }
@@ -38,8 +40,22 @@ function generateInputGrid(input) {
   }
 }
 
+function randomColor() {
+  let o = Math.round, r = Math.random, s = 255;
+  return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+}
+
+function addRandomMouseEvent() {
+  const allSquares = document.querySelectorAll(".square");
+  for (let i = 0; i < allSquares.length; i++) {
+    allSquares[i].addEventListener("mouseover", () => {
+      allSquares[i].style.backgroundColor = randomColor();
+    });
+  }
+}
+
 generateDefaultGrid();
-addMouseEvent();
+addDefaultMouseEvent();
 
 modify.addEventListener("click", () => {
   const newNumber = input.value;
@@ -49,13 +65,25 @@ modify.addEventListener("click", () => {
   if (newNumber < 2 || newNumber > 100) {
     const error = document.createElement("p");
     error.classList.add("error");
+    error.style.backgroundColor = "red";
+    error.style.color = "white";
+    error.style.padding = "5px";
+    error.style.border = "3px solid red";
+    error.style.borderRadius = "8px";
     error.textContent = "Value must be between 2 and 100!";
     body.appendChild(error);
   } else {
     document.querySelectorAll(".square").forEach(e => e.remove());
     document.querySelectorAll(".row").forEach(e => e.remove());
-    document.querySelectorAll(".blackSquare").forEach(e => e.remove());
     generateInputGrid(newNumber);
-    addMouseEvent();
+    addDefaultMouseEvent();
   }
+})
+
+randomSquares.addEventListener("click", () => {
+  addRandomMouseEvent();
+})
+
+blackSquares.addEventListener("click", () => {
+  addDefaultMouseEvent();
 })
